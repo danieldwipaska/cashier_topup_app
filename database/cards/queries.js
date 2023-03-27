a1ee0e21497eb27dc714696b88133276aae1595c
Daniel Kamasi <scientificallychristian@gmail.com>
2023-03-28 04:15:39 +0700

    add property created_at and updated_at to cards table

--- database/cards/queries.js
+++ database/cards/queries.js
@@ -1,12 +1,12 @@
-const addCard = 'INSERT INTO cards (id, barcode, balance, customer_name, is_member, is_active, dine_in) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+const addCard = 'INSERT INTO cards (id, barcode, balance, customer_name, is_member, is_active, dine_in, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, NOW(), NOW())';
 const getCards = 'SELECT * FROM cards';
 const getCardById = 'SELECT * FROM cards WHERE barcode = $1';
 // const checkEmailExists = 'SELECT s FROM students s WHERE s.email = $1';
 // const removeStudent = 'DELETE FROM students WHERE id = $1';
-const cardActivate = 'UPDATE cards SET is_active = $1 WHERE barcode = $2';
-const cardDeactivate = 'UPDATE cards SET is_active = $1, dine_in = $2, customer_name = $3, balance = $4 WHERE barcode = $5';
-const cardStatus = 'UPDATE cards SET dine_in = $1, customer_name = $2, balance = $3 WHERE barcode = $4';
-const updateBalance = 'UPDATE cards SET balance = $1 WHERE barcode = $2 RETURNING *';
+const cardActivate = 'UPDATE cards SET is_active = $1, updated_at = NOW() WHERE barcode = $2';
+const cardDeactivate = 'UPDATE cards SET is_active = $1, dine_in = $2, customer_name = $3, balance = $4, updated_at = NOW() WHERE barcode = $5';
+const cardStatus = 'UPDATE cards SET dine_in = $1, customer_name = $2, balance = $3, updated_at = NOW() WHERE barcode = $4';
+const updateBalance = 'UPDATE cards SET balance = $1, updated_at = NOW() WHERE barcode = $2 RETURNING *';
 
 module.exports = {
   addCard,
---
+++ database/cards/table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE cards (
+id UUID NOT NULL PRIMARY KEY,
+barcode VARCHAR(50) NOT NULL,
+balance INT NOT NULL,
+customer_name VARCHAR(50), 
+is_member BOOLEAN NOT NULL,
+is_active BOOLEAN NOT NULL,
+dine_in BOOLEAN NOT NULL,
+created_at TIMESTAMPTZ NOT NULL,
+updated_at TIMESTAMPTZ NOT NULL
+);
