89d7809994c2263f645e340a819703a90433ac89
Daniel Kamasi <scientificallychristian@gmail.com>
2023-08-05 14:14:04 +0700

    database config database name

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: 'localhost',
-  database: 'cafecashier',
+  database: 'bahari_card_manager',
   password: process.env.POSTGRES_PASS,
   port: 5432,
 });
