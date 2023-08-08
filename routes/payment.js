5b9d3df571ed7b504aa5664554dbb585689ae4bd
Daniel Kamasi <scientificallychristian@gmail.com>
2023-08-08 14:29:04 +0700

    edit timezone 25

--- routes/payment.js
+++ routes/payment.js
@@ -368,6 +368,9 @@ router.post('/download', async (req, res) => {
     const dateFrom = new Date(dateFromString);
     const dateTo = new Date(dateToString);
 
+    console.log(dateFrom);
+    console.log(dateTo);
+
     // const dateFromUtc = convertTZ(dateFrom, 'Asia/Jakarta');
     // const dateToUtc = convertTZ(dateTo, 'Asia/Jakarta');
 
