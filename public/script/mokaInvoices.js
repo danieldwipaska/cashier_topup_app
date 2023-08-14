f854ae3d221abed12190f9970ce5dd96bb0ac74a
Daniel Kamasi <scientificallychristian@gmail.com>
2023-08-14 10:03:18 +0700

    Connect to MokaPOS and adjusting the payment portal

--- public/script/mokaInvoices.js
+++ public/script/mokaInvoices.js
@@ -1,18 +1,23 @@
 async function getMokaInvoices() {
   // const mokaData = document.querySelector('.moka-data');
-  // mokaData.innerHTML = 'halo';
+  const mokaData = document.querySelector('.moka-data');
+  mokaData.innerHTML = `<div class="d-flex justify-content-center">
+  <div class="spinner-border text-primary" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>`;
 
   try {
     const response = await axios.get('http://localhost:3000/thirdparty/moka');
 
-    const mokaData = document.querySelector('.moka-data');
-
     let dataString = '';
 
     response.data.data.payments.forEach((element, i) => {
-      dataString += `<button type="button" class="btn btn-light shadow text-start p-3" id="${i}"><strong>${element.payment_no}</strong>&emsp;&emsp;Customer: <strong>${element.customer_name}</strong>&emsp;&emsp;IDR${Intl.NumberFormat(
-        'en-US'
-      ).format(element.total_collected)} &emsp;&emsp;Served By: <strong>${element.checkouts[0].sales_type_name}</strong></button>`;
+      const date = new Date(element.created_at);
+      const dateString = date.toLocaleString();
+      dataString += `<button type="button" class="btn btn-light shadow text-start p-3" id="${i}"><small>${dateString}</small>&emsp;&emsp;<strong>${element.payment_no}</strong>&emsp;&emsp;Customer: <strong>${
+        element.customer_name
+      }</strong>&emsp;&emsp;IDR${Intl.NumberFormat('en-US').format(element.total_collected)} &emsp;&emsp;Served By: <strong>${element.checkouts[0].sales_type_name}</strong></button>`;
 
       dataCollection.push(element);
     });
