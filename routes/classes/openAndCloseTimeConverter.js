2aed5ad28359d021978a412f7687c446262ff2b0
danieldwipaska <scientificallychristian@gmail.com>
2023-10-04 13:54:57 +0700

    testing 13

--- routes/classes/openAndCloseTimeConverter.js
+++ routes/classes/openAndCloseTimeConverter.js
@@ -9,7 +9,7 @@ class OpenAndCloseTimeConverter {
     let hourNow = date.getHours();
     // console.log(hourNow);
 
-    if (hourNow >= 0 && hourNow <= 4) {
+    if (hourNow >= 0 && hourNow <= 5) {
       hourNow = hourNow + 24;
     }
 
@@ -18,7 +18,7 @@ class OpenAndCloseTimeConverter {
 
   static open() {
     const hourNow = this.hourNow();
-    const rangeToFrom = hourNow - 5;
+    const rangeToFrom = hourNow - 6;
     // UNCERTAINTY OF 1 HOUR
     const dateFromString = Date.now() - rangeToFrom * 60 * 60 * 1000;
     const dateFromDate = new Date(dateFromString);
@@ -28,7 +28,7 @@ class OpenAndCloseTimeConverter {
 
   static close() {
     const hourNow = this.hourNow();
-    const rangeToTo = 28 - hourNow;
+    const rangeToTo = 21 - hourNow;
     const dateToString = Date.now() + rangeToTo * 60 * 60 * 1000;
     const dateToDate = new Date(dateToString);
 
