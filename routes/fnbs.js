fede42da107c94c785ff42b0f1a83998f8cfd502
danieldwipaska <scientificallychristian@gmail.com>
2023-11-11 11:37:33 +0700

    update and delete fnb

--- routes/fnbs.js
+++ routes/fnbs.js
@@ -8,7 +8,7 @@ const pool = require('../db');
 const fnbQueries = require('../database/fnbs/queries');
 const { v4 } = require('uuid');
 
-// CREATE MENU
+// CREATE FNB
 router.get('/list', verifyToken, cashierAndDeveloper, async (req, res) => {
   try {
     const fnbs = await pool.query(fnbQueries.getFnbs, []);
@@ -71,6 +71,70 @@ router.post('/add', verifyToken, allRoles, async (req, res) => {
     }
   } catch (error) {
     errorLog(fnbLogger, error, 'Error in POST / when calling fnbQueries.getFnbByCode');
+    return res.status(500).json('Server Error');
+  }
+});
+
+// GET A FNB
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const fnbs = await pool.query(fnbQueries.getFnbById, [id]);
+    if (!fnbs.rows.length) return res.status(404).json('Food / Beverages Not Found');
+
+    return res.status(200).json(fnbs.rows[0]);
+  } catch (error) {
+    errorLog(fnbLogger, error, 'Error in GET /:id when calling fnbQueries.getFnbById');
+    return res.status(500).json('Server Error');
+  }
+});
+
+// UPDATE FNB
+router.post('/:id', verifyToken, allRoles, async (req, res) => {
+  const { id } = req.params;
+  const { code, menu, price, kind } = req.body;
+
+  try {
+    const fnbs = await pool.query(fnbQueries.getFnbById, [id]);
+    if (!fnbs.rows.length) return res.status(404).json('Food / Beverages Not Found');
+
+    try {
+      await pool.query(fnbQueries.updateFnbById, [code, menu, kind, price, fnbs.rows[0].raw_mat, fnbs.rows[0].raw_amount, id]);
+
+      infoLog(fnbLogger, 'Fnb was successfully updated', '', '', '', req.validUser.name);
+
+      return res.redirect('/fnb/list');
+    } catch (error) {
+      errorLog(fnbLogger, error, 'Error in POST /:id when calling fnbQueries.updateFnbById');
+      return res.status(500).json('Server Error');
+    }
+  } catch (error) {
+    errorLog(fnbLogger, error, 'Error in POST /:id when calling fnbQueries.getFnbById');
+    return res.status(500).json('Server Error');
+  }
+});
+
+// DELETE FNB
+router.get('/:id/delete', verifyToken, allRoles, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const fnbs = await pool.query(fnbQueries.getFnbById, [id]);
+    if (!fnbs.rows.length) return res.status(404).json('Food / Beverages Not Found');
+
+    try {
+      await pool.query(fnbQueries.deleteFnbById, [id]);
+
+      infoLog(fnbLogger, 'Fnb was successfully deleted', '', '', '', req.validUser.name);
+
+      return res.redirect('/fnb/list');
+    } catch (error) {
+      errorLog(fnbLogger, error, 'Error in GET /:id/delete when calling fnbQueries.getFnbById');
+      return res.status(500).json('Server Error');
+    }
+  } catch (error) {
+    errorLog(fnbLogger, error, 'Error in GET /:id/delete when calling fnbQueries.getFnbById');
     return res.status(500).json('Server Error');
   }
 });
