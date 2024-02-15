ca8dc0a72af30db70e97a425bec929771502a838
danieldwipaska <scientificallychristian@gmail.com>
2024-02-15 23:32:39 +0700

    feat: add empty/required mark on empty input in confirmation pop-up

--- public/script/topUpScript.js
+++ public/script/topUpScript.js
@@ -51,6 +51,13 @@ topUpButton.addEventListener('click', () => {
   console.log('payment method', paymentMethodInput.value);
   console.log('notes', notesInput.value);
   console.log('----------- Confirmed Values -----------');
+  console.log('barcode', barcode.value);
+  console.log('add balance', addBalance.value);
+  console.log('deposit', deposit.value);
+  console.log('customer name', customerName.value);
+  console.log('customer id', customerId.value);
+  console.log('payment method', paymentMethod.value);
+  console.log('notes', notes.value);
 
   confirmTopupButton.removeAttribute('disabled');
 
@@ -75,15 +82,29 @@ topUpButton.addEventListener('click', () => {
   subTotalDisplay.innerHTML = addBalanceInput.value;
   totalDisplay.innerHTML = addBalanceInput.value;
 
-  if (!addBalance.value || !customerName.value || !customerId.value || !paymentMethod.value) {
+  addBalanceDisplay.classList.remove('text-danger');
+  customerNameDisplay.classList.remove('text-danger');
+  customerIdDisplay.classList.remove('text-danger');
+  paymentMethodDisplay.classList.remove('text-danger');
+
+  if (!addBalance.value || addBalance.value === '0') {
+    addBalanceDisplay.innerHTML = 'Required';
+    addBalanceDisplay.classList.add('text-danger');
     confirmTopupButton.setAttribute('disabled', 'disabled');
   }
-
-  console.log('barcode', barcode.value);
-  console.log('add balance', addBalance.value);
-  console.log('deposit', deposit.value);
-  console.log('customer name', customerName.value);
-  console.log('customer id', customerId.value);
-  console.log('payment method', paymentMethod.value);
-  console.log('notes', notes.value);
+  if (!customerName.value) {
+    customerNameDisplay.innerHTML = 'Required';
+    customerNameDisplay.classList.add('text-danger');
+    confirmTopupButton.setAttribute('disabled', 'disabled');
+  }
+  if (!customerId.value) {
+    customerIdDisplay.innerHTML = 'Required';
+    customerIdDisplay.classList.add('text-danger');
+    confirmTopupButton.setAttribute('disabled', 'disabled');
+  }
+  if (!paymentMethod.value) {
+    paymentMethodDisplay.innerHTML = 'Required';
+    paymentMethodDisplay.classList.add('text-danger');
+    confirmTopupButton.setAttribute('disabled', 'disabled');
+  }
 });
