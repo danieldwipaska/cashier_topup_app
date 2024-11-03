47f7ef8f11fe43b5fa5d31458bf4a37b5d4c206f
Daniel Kamasi <danielkamasi@outlook.com>
2024-11-03 13:04:59 +0700

    testing

--- routes/middlewares/userRole.js
+++ routes/middlewares/userRole.js
@@ -1,11 +1,9 @@
 const verifyToken = require('./verifyToken');
-const Sentry = require('@sentry/node');
 
 function developerOnly(req, res, next) {
   const userPosition = req.validUser.position;
 
   if (userPosition !== 'developer') {
-    Sentry.captureException(new Error('Non-developer is trying to access developer-only route'));
     return res.status(402).json('You have no access to this page');
   } else {
     next();
