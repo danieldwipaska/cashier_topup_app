298d6eb5391bfdb3a889dfb437457f3c1c3d9e53
Daniel Kamasi <danielkamasi@outlook.com>
2025-02-02 22:52:55 +0700

    debug home 10

--- routes/api/moka.js
+++ routes/api/moka.js
@@ -30,6 +30,7 @@ async function getFirstAuth() {
     }
   } catch (error) {
     errorLog(mokaLogger, error, 'Error in function getFirstAuth() when calling axios');
+    console.log(error);
   }
 }
 
